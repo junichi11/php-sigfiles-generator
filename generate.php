883620be2ade41aff65ad16ef4d86969f498e9b1
junichi11 <junichi0206@gmail.com>
2018-10-11 10:02:06 +0900

    Add a script to run the php lint command for php files of the output directory
    - Run `php verify-syntax.php` after sigfiles are generated

--- generate.php
+++ generate.php
@@ -1,24 +1,11 @@
 <?php
 
+require './bootstrap.php';
+
 use utils\Files;
 use utils\Log;
 
-define('NEW_LINE', "\n");
-spl_autoload_register(function($class) {
-    $classFile = __DIR__ . '/src/' . str_replace('\\', '/', $class) . '.php';
-    if (is_file($classFile)) {
-        require_once $classFile;
-    } else {
-        Log::error("Cannot find class file for '$class'", true);
-    }
-});
-
 Log::start(false);
-Config::create([
-    __DIR__ . '/config.ini',
-    __DIR__ . '/config.local.ini',
-    getenv('HOME') . '/.php.sigfiles.config.ini',
-]);
 
 // clean
 Files::deleteFiles(Config::get()->outputDir());
